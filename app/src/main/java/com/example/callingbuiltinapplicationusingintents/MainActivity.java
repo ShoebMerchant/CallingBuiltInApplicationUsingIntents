f87bf609d4aa52fca4c8313447b40a9045671353
vm-shoeb <shoeb.merchant@purplepatch.in>
2023-05-06 03:19:59 +0530

    Operations Added on Buttons Explicit and Implicit Intent integrated

--- app/src/main/java/com/example/callingbuiltinapplicationusingintents/MainActivity.java
+++ app/src/main/java/com/example/callingbuiltinapplicationusingintents/MainActivity.java
@@ -18,45 +18,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnWeb = (Button) findViewById(R.id.btnWeb);
-        btnWeb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.amazon.com"));
-                startActivity(i);
-            }
-        });
+//        btnWeb = (Button) findViewById(R.id.btnWeb);
+//        btnWeb.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.amazon.com"));
+//                startActivity(i);
+//            }
+//        });
+//
+//        btnCall = (Button) findViewById(R.id.btnCall);
+//        btnCall.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(
+//                        Intent.ACTION_DIAL,
+//                        Uri.parse("tel:+919561463128"));
+//                startActivity(i);
+//            }
+//        });
+//
+//        btnMap = (Button) findViewById(R.id.btnMap);
+//        btnMap.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(
+//                        Intent.ACTION_VIEW,
+//                        Uri.parse("geo:37.827500,-122.481670"));
+//                startActivity(i);
+//            }
+//        });
 
-        btnCall = (Button) findViewById(R.id.btnCall);
-        btnCall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(
-                        Intent.ACTION_DIAL,
-                        Uri.parse("tel:+919561463128"));
-                startActivity(i);
-            }
-        });
 
-        btnMap = (Button) findViewById(R.id.btnMap);
-        btnMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("geo:37.827500,-122.481670"));
-                startActivity(i);
-            }
-        });
 
-        btnContact = (Button) findViewById(R.id.btnContact);
-        btnContact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_PICK);
-                i.setType(ContactsContract.Contacts.CONTENT_TYPE);
-                startActivityForResult(i, request_Code);
-            }
-        });
+//        Button btnActivity2 = findViewById(R.id.btnActivity2);
+//        btnActivity2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(MainActivity.this, Activity2.class);
+//                startActivity(i);
+//            }
+//        });
+    }
+
+    public void operations(View view) {
+        Button btn = (Button) view;
+        if(btn.getId()==R.id.btnActivity2){
+            startActivity(new Intent(MainActivity.this,Activity2.class));
+        }
+        if(btn.getId()==R.id.btnContact){
+            Intent i = new Intent(Intent.ACTION_PICK);
+            i.setType(ContactsContract.Contacts.CONTENT_TYPE);
+            startActivityForResult(i, request_Code);
+        }
+        if(btn.getId()==R.id.btnMap){
+            Intent i = new Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("geo:37.827500,-122.481670"));
+            startActivity(i);
+        }
+        if(btn.getId()==R.id.btnCall){
+            Intent i = new Intent(
+                    Intent.ACTION_DIAL,
+                    Uri.parse("tel:+919561463128"));
+            startActivity(i);
+        }
+        if(btn.getId()==R.id.btnWeb){
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.amazon.com"));
+            startActivity(i);
+        }
     }
 }
